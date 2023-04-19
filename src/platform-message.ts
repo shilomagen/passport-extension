f855b8edde889bd1fe737a979c233764e2a616cf
Nitzan Mousan <nitzan.mousan@fiverr.com>
2023-04-19 23:38:53 +0300

    :fire: removing unused REPORT_ANALYTICS message type from action types

--- src/platform-message.ts
+++ src/platform-message.ts
@@ -2,7 +2,6 @@ export enum ActionTypes {
   IsLoggedIn = 'IS_LOGGED_IN',
   StartSearch = 'START_SEARCH',
   StopSearch = 'STOP_SEARCH',
-  ReportAnalytics = 'REPORT_ANALYTICS',
 }
 
 interface IsLoggedInMessage {
