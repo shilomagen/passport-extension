c9bfbde3f49501f4828916ea20a1e8db984a7562
Shilo Magen <smangam@gmail.com>
2023-04-20 19:00:53 +0300

    Remove unnecessary headers

--- src/lib/http.ts
+++ src/lib/http.ts
@@ -55,8 +55,6 @@ export class HttpService {
         'application-api-key': 'D7662A08-48D1-4BC8-9E45-7F9DDF8987E3',
         'application-name': 'PibaV1',
         'accept-language': 'en',
-        pragma: 'no-cache',
-        'cache-control': 'no-cache',
       },
       withCredentials: true,
     });
